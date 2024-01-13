4e95937f21e2d79b70c596de3f82589cfb0bc8e2
kulbhushanshakya <tech.kulbhushan@gmail.com>
2024-01-14 01:52:05 +0530

    first commit

---
+++ src/main/java/com/cg/springbootazuredemos/SpringBootAzureDemosApplication.java
@@ -0,0 +1,21 @@
+package com.cg.springbootazuredemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAzureDemosApplication {
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return "Azure App deployed successfully";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAzureDemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cg/springbootazuredemos/SpringBootAzureDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.springbootazuredemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAzureDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
