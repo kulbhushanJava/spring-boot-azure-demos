b1644d87d594560bab8ecbd7dc4ceb8d857074fd
kulbhushanshakya <tech.kulbhushan@gmail.com>
2024-01-15 14:47:58 +0530

    second new commit

--- src/main/java/com/cg/springbootazuredemos/SpringBootAzureDemosApplication.java
+++ src/main/java/com/cg/springbootazuredemos/SpringBootAzureDemosApplication.java
@@ -11,7 +11,7 @@ public class SpringBootAzureDemosApplication {
 	@GetMapping("/message")
 	public String getMessage()
 	{
-		return "Azure App deployed successfully";
+		return "Azure Application deployed successfully";
 	}
 
 	public static void main(String[] args) {
